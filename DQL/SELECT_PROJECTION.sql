0053c1d2be7bb46fe4eda87a3c42dfea8fb86d32
Mack-0409 <mayanksingh0409@gmail.com>
2026-01-04 00:05:40 +0530

    SQL

---
+++ DQL/SELECT_PROJECTION.sql
@@ -0,0 +1,16 @@
+-- -- PROJECTION SYNTAX 
+-- SELECT * / [DISTINCT] COLUMN_NAME / EXPRESSION [ALIAS]
+-- FROM TABLE_NAME ;
+
+-- ORDER OF EXECUTION
+-- 1. FROM
+-- 2. SELECT
+
+-- -- WAQTD the names of the student
+-- SELECT *
+-- FROM EMP;
+
+-- -- WAQTD  Hiredate, Comm, Salary of the employee
+-- SELECT HIREDATE, COMM, Sal
+-- FROM EMP;
+
