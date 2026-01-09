f8d4b35c89cfd71eb6065b87cfa3327036191383
Mack-0409 <mayanksingh0409@gmail.com>
2026-01-09 21:15:59 +0530

    ALIAS

--- DQL/SELECT_PROJECTION.sql
+++ DQL/SELECT_PROJECTION.sql
@@ -25,3 +25,32 @@
 -- 1. Operand [column name]
 -- 2. Operator 
 
+-- -- WAQTD Annual salary of the eemployee
+-- SELECT SAL * 12 
+-- FROM EMP;
+
+-- -- WAQTD half term salary of employee and 300rs bonous in the salary
+-- SELECT SAL * 6, SAL + 300
+-- FROM EMP; 
+
+-- -- WAQTD 30 percent increment in the salary
+-- SELECT SAL * 0.3
+-- FROM EMP;
+-- [OR]
+-- SELECT SAL + SAL * 30/100
+-- FROM EMP;
+
+-- -- WAQTD 20 percent decrement in the salary along with half term salary with 200rs increment and 5 percent increment in the salary
+-- SELECT SAL - SAL * 0.2, SAL * 6 + 200, SAL + SAL * 0.05
+-- FROM EMP;
+
+-- # ALIAS 
+-- It is an alternative name given to an column or expression int the result table.
+-- It can be used with or without AS keyword.
+-- ALIAS name should be single word or it should be enclosed with in double quotes ("") or we can use underscore (_) if it is having multiple words.
+-- It is not mandatory but highly recommended
+-- Example :- 
+-- 1. SAL * 12 AS ANNUALSALARY
+-- 2. SAL * 12 ANNUALSALARY
+-- 3. SAL * 12 "ANNUAL SALARY"
+-- 4. SAL * 12 ANNUAL_SALARY
