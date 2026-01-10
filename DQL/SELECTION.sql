67f297de123b5faa37d31e1ffa561e7c170ec66c
Mack-0409 <mayanksingh0409@gmail.com>
2026-01-11 00:02:00 +0530

    SELECTION

---
+++ DQL/SELECTION.sql
@@ -0,0 +1,13 @@
+-- SELECTION 
+-- It is used to retrive the data from the table by selecting both rows and columns.
+
+-- -- SYNTAX
+-- SELECT */[DISTINCT] COLUMN_NAME / EXPRESSION [ALIAS]
+-- FROM TABLE_NAME
+-- WHERE <FILTER CONDITION>;
+
+-- -- ORDER OF EXECUTION
+-- 1. FROM
+-- 2. WHERE
+-- 3. SELECT
+
