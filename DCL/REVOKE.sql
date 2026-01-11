d86b0f734a376657630af7fa8718c9ba82380c9e
Mack-0409 <mayanksingh0409@gmail.com>
2026-01-11 22:09:15 +0530

    DCL

---
+++ DCL/REVOKE.sql
@@ -0,0 +1,15 @@
+-- DCL [Data Control Language]
+-- This statement is used to control the flow of data.
+
+-- There are 2 types of statements
+-- 1. GRANT
+-- 2. REVOKE
+
+-- 2. REVOKE 
+--       It is used to take back the given permission from user.
+
+-- SYNTAX - REVOKE SQL_STATEMENT ON TABLE_NAME
+--          FROM USER_NAME;   
+
+-- EXAMPLE - REVOKE SELECT ON EMP
+--           FROM HR;  
