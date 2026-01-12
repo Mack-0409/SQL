71f82163feaa02cae708c834ac8e4d522d0681af
Mack-0409 <mayanksingh0409@gmail.com>
2026-01-12 20:01:00 +0530

    TCL

---
+++ TCL/COMMIT.sql
@@ -0,0 +1,20 @@
+-- TCL [Transaction Control Language]
+-- It is used to control the transaction done on the database.
+
+-- Transaction - The DML operations perform on the database is called as transaction such as insertion, updating and deletion of the records.
+
+-- Thre are 3 types in TCL
+-- 1. COMMIT
+-- 2. ROLLBACK
+-- 3. SAVE POINT 
+
+
+-- 1. COMMIT 
+    -- It is used to save the records transaction on the database
+
+-- SYNTAX - COMMIT;
+
+-- EXAMPLE - INSERT INTO EMP VALUES (1,'ARYA')
+--           INSERT INTO EMP VALUES (2,'MAYA')
+--           INSERT INTO EMP VALUES (3,'JAYA')  
+--           COMMIT;
---
+++ TCL/SAVEPOINT.sql
@@ -0,0 +1,28 @@
+-- TCL [Transaction Control Language]
+-- It is used to control the transaction done on the database.
+
+-- Transaction - The DML operations perform on the database is called as transaction such as insertion, updating and deletion of the records.
+
+-- Thre are 3 types in TCL
+-- 1. COMMIT
+-- 2. ROLLBACK
+-- 3. SAVE POINT 
+
+
+
+-- 3. SAVE POINT
+    -- It is used to give the restoration point or mark the position or checkpoint
+
+-- SYNTAX - SAVE POINT SAVEPOINT_NAME;
+
+-- EXAMPLE - SAVE POINT SAVEPOINT_NAME;
+--           SAVE POINT S1;
+--           ROLLBACK TO SAVEPOINT_NAME;
+
+--           RECORDS ARE INSERTED_SAVEPOINT S1;
+--           RECORDS ARE INSERTED_SAVEPOINT S2;
+--           RECORDS ARE INSERTED_SAVEPOINT S3;
+
+--           COMMAND ROLLBACK TO S3;
+--         --   ALL 30 RECORDS WILL BE DISPLAYED
+--           ROLLBACK TO S2;  
