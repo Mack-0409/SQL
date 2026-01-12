dd715393272528af8c09d4231778e4f246a4fdd7
Mack-0409 <mayanksingh0409@gmail.com>
2026-01-12 21:03:59 +0530

    DDL

---
+++ DDL/TRUNCATE.sql
@@ -0,0 +1,10 @@
+-- DDL [Data Definition Language]
+--     This statement is used to construct, modify and remove an object
+
+-- There are 5 statements in DDL
+
+-- 1. CREATE
+-- 2. RENAME
+-- 3. ALTER
+-- 4. TRUNCATE
+-- 5. DROP
