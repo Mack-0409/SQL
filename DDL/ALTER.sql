38660d7c39bd6670d10f1d3c6b57a62370123b92
Mack-0409 <mayanksingh0409@gmail.com>
2026-01-13 23:36:26 +0530

    DDL

--- DDL/ALTER.sql
+++ DDL/ALTER.sql
@@ -8,3 +8,37 @@
 -- 3. ALTER
 -- 4. TRUNCATE
 -- 5. DROP
+
+
+
+
+-- 3. ALTER
+--     This statement is used to modify the existing object
+
+-- 1. To add a column
+--    SYNTAX - ALTER TABLE TABLE_NAME
+--             ADD COLUMN_NAME DATATYPE CONSTRAINTS TYPES;
+
+-- 2. To drop a column 
+--    SYNTAX - ALTER TABLE TABLE_NAME
+--             DROP COLUMN COLUMN_NAME;
+
+-- 3. To rename a column
+--    SYNTAX - ALTER TABLE TABLE_NAME
+--             RENAME COLUMN COLUMN_NAME TO NEW_COLUMN NAME;
+
+-- 4. To modify datatypes
+--    SYNTAX - ALTER TABLE TABLE_NAME
+--             MODIFY COLUMN_NAME NEW  DATATYPE;
+
+-- 5. To modify constraints
+--    SYNTAX - ALTER TABLE TABLE_NAME
+--    MODIFY COLUMN_NAME EXISTING DATATYPE CONSTRAINTS;              
+
+-- NOTE - 
+
+-- To add foreign key
+
+-- ALTER TABLE TABLE_NAME
+-- ADD CONSTRAINTS [REF_NAME] FOREIGN KEY (COLUMN_NAME)
+-- PARENT_TABLE_NAME (COLUMN_NAME);
